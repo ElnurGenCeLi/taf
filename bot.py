23e823a221a1a8bc50a7217f62d0ac7b3e1ff2eb
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2022-07-21 22:17:26 +0400

    update bot.py

--- bot.py
+++ bot.py
@@ -553,5 +553,5 @@ async def mentionall(tagadmin):
 		sleep(0.5)
 
 
-print(">> Bot aktifdi bot hakda məlumatı\n@ElnurGenCeLi dan ala bilərsən Versiya 1.7.5<<")
+print("Bot aktifdi bot hakda məlumatı @ElnurGenCeLi dan ala bilərsən Versiya 1.7.5")
 client.run_until_disconnected()
