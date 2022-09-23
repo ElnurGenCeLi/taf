5dcf209289d830da05869c60d58cabcc38fac1dc
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2022-09-23 14:02:40 +0400

    update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def help(event):
   helptext = "**[Karona](https://t.me/KaronaTaggerRobot) Butonları 🤖**\n\n**/sehidler - ŞƏHİDLƏRİMİZİN ADLARI İLƏ ÇAĞIRAR\n**/cancel - botu dayandırar**\n**/tag <səbəb> - 5-li tag edər**\n**/etag <səbəb> - Emoji ilə tag edərr**\n**/mtag <səbəb> - mafia rolları ilə tag edər\n**/tektag <səbəb> - İstifadəçiləri tək tək tag edər\n**/admins <səbəb> - Yönəticiləri tək tək tag edər\n**/btag <səbəb> - Bayrağla tag edər**\n/stag <səbəb> - Şəhərlərimizin adları ilə tag edər"
 	
 @client.on(events.NewMessage(pattern="^/qurup$"))
-async def help(event):
+async def qurub(event):
   helptext = "Premium Söhbət Qurupları ⚡\n\nƏlaqə - @ElnurGenCeLi"
   await event.reply(helptext,
                     buttons=(
